75cd3c3b34779b4e3c8cc9e65232299065cd5cfb
takbirgazi <takbirgazibd@gmail.com>
2024-06-06 17:54:42 +0600

    change base URL in asios

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://geographyolympiadbd-server.vercel.app"
 })
 const useAxiosSecure = () => {
     return axiosSecure;
